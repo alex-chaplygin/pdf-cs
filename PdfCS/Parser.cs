f9bc4de030eb6605d41155f8f02081f7f994b53b
alex-chaplygin <alex_chaplygin@mail.ru>
2021-09-16 10:31:11 +0300

    Парсер: пропуск пустых символов

---
+++ PdfCS/Parser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PdfCS
+{
+    class Parser
+    {
+        private Stream stream;
+        private char lastChar;
+
+        public Parser(Stream s)
+        {
+            stream = s;
+        }
+
+        public char NextChar()
+        {
+            lastChar = (char)stream.ReadByte();
+            return lastChar;
+        }
+
+        public bool isWhiteSpace(char c)
+        {
+            string whiteSpaces = "\x00\t\r\n \x0c";
+
+            return whiteSpaces.IndexOf(c) != -1;
+        }
+
+        public bool isDelimeter(char c)
+        {
+            string delimeters = "()<>{}[]/%";
+
+            return delimeters.IndexOf(c) != -1;
+        }
+
+        public void SkipComment()
+        {
+            while (NextChar() != '\n') ;
+        }
+
+        public void SkipWhitespace()
+        {
+            while (lastChar == '%' || isWhiteSpace(lastChar))
+            {
+                if (lastChar == '%') SkipComment();
+                NextChar();
+            }
+        }
+    }
+}
