e6d10ef0836a93b7b4d2da64e1287e8d8b6c2eec
alex-chaplygin <alex_chaplygin@mail.ru>
2021-09-28 06:40:10 +0300

    Тестирование ASCII85

--- PdfCS/Program.cs
+++ PdfCS/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace PdfCS
 {
@@ -10,6 +11,14 @@ namespace PdfCS
     {
         static void Main(string[] args)
         {
+	    Print(ASCII85.Decode(File.ReadAllBytes(args[0])));
+        }
+
+	static void Print(byte[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write((char)arr[i]);
+	    Console.WriteLine();
         }
     }
 }
