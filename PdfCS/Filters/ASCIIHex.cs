292224870f5bcca35782af8bfdd3bb3ff3ee5ce4
alex-chaplygin <alex_chaplygin@mail.ru>
2021-09-30 07:40:49 +0300

    Декодирование ASCIIHex

---
+++ PdfCS/Filters/ASCIIHex.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PdfCS
+{
+    class ASCIIHex
+    {
+        public static byte[] Decode(byte[] stream)
+        {
+            string s = "";
+            List<byte> bt = new List<byte>();
+            for (int i = 0; i < stream.Length; i++)
+            {
+                char c = (char)stream[i];
+                if (c == '>')
+                    break;
+                else if (Parser.IsWhitespace(c))
+                    continue;
+                else if (Char.IsDigit(c) || c >= 'a' && c <= 'f' || c >= 'A' && c <= 'F')
+                    s += c;
+                else
+                    throw new Exception($"Неверный символ ASCIIHex: {c}");
+                if (s.Length == 2)
+                {
+                    bt.Add(Convert.ToByte(s, 16));
+                    s = "";
+                }
+            }
+            if (s != "")
+                bt.Add(Convert.ToByte(s, 16));
+            return bt.ToArray();
+        }
+    }
+}
