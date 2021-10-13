af633868b6e73aff5cdcc756aa85d8f142a223b8
alex-chaplygin <alex_chaplygin@mail.ru>
2021-10-13 16:32:21 +0300

    Класс восстановления изображения по предсказанию

---
+++ PdfCS/Filters/Predictor.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfCS
+{
+    /// <summary>
+    ///   Класс восстановления изображения по предсказанию
+    /// </summary>
+    public class Predictor
+    {
+	public static byte[] Decode(byte[] data, int predictor, int colors, int bpp, int columns)
+	{
+	    return data;
+	}
+    }
+}
+      
