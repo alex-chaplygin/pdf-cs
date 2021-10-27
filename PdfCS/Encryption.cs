3c1dcd7480a6b1f4652bbf95adf268befe74c3c8
alex-chaplygin <alex_chaplygin@mail.ru>
2021-10-27 10:18:27 +0300

    Класс дешифрования документа

---
+++ PdfCS/Encryption.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfCS
+{
+    /// <summary>
+    ///   Класс шифрования для потоковых объектов
+    /// </summary>
+    public class Encryption
+    {
+        /// <summary>
+        ///   Применяет фильтр для дешифрования потока
+        /// </summary>
+        /// <param name="stream">Зашифрованный поток</param>
+        /// <param name="name">Имя фильтра (если Identity - то без шифрования)</param>
+        /// <param name="params">Словарь параметров фильтра</param>
+        /// <returns>
+        /// Дешифрованный поток как массив байт
+        /// </returns>
+        public static byte[] ApplyFilter(byte[] stream, string name, Dictionary<string, object> param)
+        {
+            return stream;
+        }
+    }
+}
