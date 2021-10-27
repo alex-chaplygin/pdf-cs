26e2f7c1189f9e957c3e265cd944c32fcf99beed
alex-chaplygin <alex_chaplygin@mail.ru>
2021-10-27 10:19:35 +0300

    Класс фильтра для дешифрования потоков

---
+++ PdfCS/Filters/Crypt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfCS
+{
+    /// <summary>
+    ///   Класс дешифрования для потоковых объектов
+    /// </summary>
+    public class Crypt
+    {
+        /// </summary>
+        ///   Декодирует поток с помощью указанного фильтра с помощью класса Encryption
+        /// </summary>
+        /// <param name="stream">Поток для декодирования</param>
+        /// <param name="param">Словарь с параметрами</param>
+        /// <returns>
+        /// Дешифрованный поток как массив байт
+        /// </returns>
+        public static byte[] Decode(byte[] stream, Dictionary<string, object> param)
+        {
+            if (!param.ContainsKey("Name"))
+                param["Name"] = "Identity";
+            return Encryption.ApplyFilter(stream, param["Name"].ToString(), param);
+        }
+    }
+}
