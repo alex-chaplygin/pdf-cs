8aeb30172db700a131bf3474e50d04ac6f4d5b7a
alex-chaplygin <alex_chaplygin@mail.ru>
2021-11-04 08:54:17 +0300

    Фильтр JPX

---
+++ PdfCS/Filters/JPEG2000/JPX.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PdfCS
+{
+    ///<summary>
+    ///Класс,представляющий сущность форматa изображения JPX
+    /// </summary>
+    public class JPX
+    {
+        ///<summary>
+        ///Основной метод декодировки байт для формата JPX
+        ///</summary>
+        ///<param name="stream">Массив байт(поток) поступающий в метод на декодировку</param>
+        public static byte[] Decode(byte[] stream)
+        {
+            throw new Exception ("Данный класс пока не реализован и находится в разработке. Хорошего вам дня :)");
+        }
+    }
+}
