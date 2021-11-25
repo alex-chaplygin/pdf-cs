5f98c1dc1b6a91eae5d21f20c8c4cfa44f0524f5
alex-chaplygin <alex_chaplygin@mail.ru>
2021-11-25 10:34:15 +0300

    Заготовка Matrix

---
+++ PdfCS/Matrix.cs
@@ -0,0 +1,13 @@
+﻿namespace PdfCS
+{
+    public class Matrix
+    {
+        public Matrix()
+	{
+	}
+	
+        public Matrix(double a, double b, double c, double d, double e, double f)
+        {
+        }
+    }
+}
