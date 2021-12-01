d08b0ad9b76bd3a4c17415b2319104de2a45c0a8
alex-chaplygin <alex_chaplygin@mail.ru>
2021-12-01 07:07:13 +0300

    Класс матрицы

--- PdfCS/Matrix.cs
+++ PdfCS/Matrix.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PdfCS.PDFGraphics
+namespace PdfCS
 {
     /// <summary>
     /// Класс для представления 
--- Test/UnitTesting.cs
+++ Test/UnitTesting.cs
@@ -44,4 +44,36 @@ namespace Microsoft.VisualStudio.TestTools.UnitTesting
 		    Fail();
 	}
     }
+
+    public class CollectionAssert
+    {
+	public static void Fail(string msg)
+	{
+	    throw new Exception("Fail " + msg);
+	}
+
+	public static void AreEqual(object o, object o2)
+	{
+	    if (o is byte[])
+	    {
+		byte[] oa = (byte[])o;
+		byte[] oa2 = (byte[])o2;
+		if (oa.Length != oa2.Length)
+		    Fail("length");
+		for (int i = 0; i < oa.Length; i++)
+		    if (oa[i] != oa2[i])
+			Fail("i = " + i + " a1 = " + oa[i] + " a2 = " + oa2[i]);
+	    }
+	    else if (o is char[])
+	    {
+		char[] oa = (char[])o;
+		char[] oa2 = (char[])o2;
+		if (oa.Length != oa2.Length)
+		    Fail("length");
+		for (int i = 0; i < oa.Length; i++)
+		    if (oa[i] != oa2[i])
+			Fail("i = " + i + " a1 = " + oa[i] + " a2 = " + oa2[i]);
+	    }
+	}
+    }
 }
