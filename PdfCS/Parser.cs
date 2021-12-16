8df2890b97cd2eb64db3432ea6f36f5a01d85334
alex-chaplygin <alex_chaplygin@mail.ru>
2021-12-16 11:11:26 +0300

    Чтение объекта - потока

--- PdfCS/Parser.cs
+++ PdfCS/Parser.cs
@@ -585,16 +585,144 @@ namespace PdfCS
                 throw new Exception("Ожидался параметр endobj");
         }
 
-        /// <summary>
-        /// Заглушка
+	/// <summary>
+        /// Читает поток из файла (последовательность байт)
+        /// 
+        /// Поток записан в файле так:
+        /// словарь #28
+        /// stream
+        /// 0 или более байт
+        /// endstream
+        /// Ключевое слово stream уже прочитано
+        /// После stream следует перевод строки, его пропускаем #2
+        /// После чего следует поток байт до слова endstream, 
+        /// в словаре указывается точное число байт,
+        /// перед endstream также есть перевод строки, 
+        /// который не включается в последовательность байт.
+        /// Поток может содержаться во внешнем файле, 
+        /// который указывается в словаре, 
+        /// тогда все байты до endstream игнорируются.
+        /// В словаре обязательно содержится 
+        /// поле Length - число закодированных байт, не включая перевод строки.
+        /// Если число данных не соответствует длине, должна выдаваться ошибка.
+        /// Остальные поля могут быть или нет.
+        /// Поле Filter содержит один (объект имя) или 
+        /// последовательность фильтров(массив имен), 
+        /// которые нужно применить в указанном порядке.
+        /// Поле DecodeParams содержит словарь или массив параметров к фильтрам.
+        /// Если фильтр один, то параметры - в виде словаря.Если фильтров много, 
+        /// то указывается массив содержащий по одному значению 
+        /// для фильтров: или null (если у фильтра нет параметров) или словарь параметров.
+        /// Если для всех фильтров не нужны параметры, то этого поля может не быть.
+        /// Поле F указывает имя файла с внешними данными, 
+        /// поле Length хранит длину данных, 
+        /// поле FFilter - фильтры, FDecodeParams - параметры.
+        /// Фильтры (параметры)
+        /// ASCIIHexDecode no
+        /// ASCII85Decode no
+        /// LZWDecode yes
+        /// FlateDecode yes
+        /// RunLengthDecode no
+        /// CCITTFaxDecode yes
+        /// JBIG2Decode yes
+        /// DCTDecode yes
+        /// JPXDecode no
+        /// Crypt yes
         /// </summary>
-        /// <param name="dict"></param>
-        /// <returns></returns>
+        /// <param name="dict">данные из объекта (словарь)</param>
+        /// <returns>массив байт, полученный после применения всех фильтров</returns>
         public byte[] ReadStream(Dictionary<string, object> dict)
         {
-            byte[] vs = new byte[0];
+            object filters = null;
+            object parameters = null;
+            List<string> sfilters = new List<string>();
+            List<Dictionary<string, object>> sparams = new List<Dictionary<string, object>>();
 
-            return vs;
+            SkipEndOfLine();
+            if (!dict.ContainsKey("Length"))
+                throw new Exception("Отсуствует длина потока");
+
+            byte[] array = new byte[(int)dict["Length"]];
+
+            if (dict.ContainsKey("F"))
+                using (FileStream fileStream = File.OpenRead((string)dict["F"]))
+                {
+                    fileStream.Read(array, 0, array.Length);
+                }
+            else
+            {
+//                Console.WriteLine($"pos = {stream.Position} lastChar = {lastChar}");
+		array[0] = (byte)lastChar;
+                stream.Read(array, 1, array.Length - 1);
+  //              Console.WriteLine($"array = {array[0]} {array.Length}");
+                SkipEndOfLine();
+    //            Console.WriteLine($"pos = {stream.Position}");
+            }
+            NextChar();
+            if ((string)ReadToken() != "endstream")
+                throw new Exception("Ожидался конец потока");
+
+            if (dict.ContainsKey("F") && dict.ContainsKey("FFilter"))
+                filters = dict["FFilter"];
+            else if (dict.ContainsKey("Filter"))
+                filters = dict["Filter"];
+            if (filters != null && filters is object[])
+                foreach (object o in (object[])filters)
+                    sfilters.Add((string)o);
+            else
+                sfilters.Add((string)filters);
+            if (dict.ContainsKey("F") && dict.ContainsKey("FDecodeParams"))
+                parameters = dict["FDecodeParams"];
+            else if (dict.ContainsKey("DecodeParams"))
+                parameters = dict["DecodeParams"];
+            if (parameters != null && parameters is object[])
+                foreach (object o in (object[])parameters)
+                    sparams.Add((Dictionary<string, object>)o);
+            else
+                sparams.Add((Dictionary<string, object>)filters);
+            array = ApplyFilter(array, sfilters, sparams);
+            return array;
+        }
+
+        /// <summary>
+        /// Применение фильтра (для метода ReadStream)
+        /// 
+        /// Пояснение.
+        /// Если будет один фильтр цикл выполниться один раз с выбором нужного фильтра.
+        /// Если фильтров много выполняться все, которые совпадут.
+        /// Если фильтров нет, цикл не запуститься и вернет исходные данные.
+        /// </summary>
+        /// <param name="data">поток</param>
+        /// <param name="filters">фильтр</param>
+        /// <param name="p">словарь параметров</param>
+        /// <returns>Отфильтрованный поток</returns>
+        private static byte[] ApplyFilter(byte[] data, List<string> filters, List<Dictionary<string, object>> p)
+        {
+            if (filters == null)
+                return data;
+            for (int i = 0; i < filters.Count; i++)
+                if (filters[i] == "ASCIIHexDecode")
+                    data = ASCIIHex.Decode(data);
+                else if (filters[i] == "ASCII85Decode")
+                    data = ASCII85.Decode(data);
+                else if (filters[i] == "LZWDecode")
+                    data = LZW.Decode(data, p[i]);
+                else if (filters[i] == "FlateDecode")
+                    data = Flate.Decode(data, p[i]);
+                else if (filters[i] == "RunLengthDecode")
+                    data = RunLength.Decode(data);
+                else if (filters[i] == "CCITTFaxDecode")
+                    data = CCITTFax.Decode(data, p[i]);
+                else if (filters[i] == "JBIG2Decode")
+                    data = JBIG2.Decode(data, p[i]);
+                else if (filters[i] == "DCTDecode")
+                    data = DCT.Decode(data, p[i]);
+                else if (filters[i] == "JPXDecode")
+                    data = JPX.Decode(data);
+                else if (filters[i] == "Crypt")
+                    data = Crypt.Decode(data, p[i]);
+
+            return data;
         }
     }
 }
