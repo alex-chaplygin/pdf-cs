9f450e5ab28906a3fbd3811f50f583b5c474962d
alex-chaplygin <alex_chaplygin@mail.ru>
2022-01-03 16:08:47 +0300

    Объект - имя

---
+++ PdfCS/NameObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfCS
+{
+    /// <summary>
+    ///   Объект - имя
+    /// </summary>
+    public class NameObject
+    {
+        private string name;
+	
+        public NameObject(string name)
+	{
+                this.name = name;
+	}
+	
+        public override bool Equals(object obj)
+        {
+	    NameObject o = (NameObject)obj;
+            return name == o.name;
+        }
+    }
+}
