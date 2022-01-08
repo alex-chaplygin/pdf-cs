e2a2cd83d7731d0ac1f012231749b39cf07d76d7
alex-chaplygin <alex_chaplygin@mail.ru>
2022-01-08 10:42:27 +0300

    Тестирование Predictor

---
+++ Test/PredictorTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using PdfCS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PDFTest
+{
+    [TestClass]
+    public class PredictorTest
+    {
+        public void TestSequences(byte[] encodedSequence, byte[] decodedSequence)
+        {
+            CollectionAssert.AreEqual(Predictor.Decode(encodedSequence, 1, 1, 1, 3), decodedSequence);
+        }
+
+        [TestMethod]
+        public void CorrectCase()
+        {
+            TestSequences(new byte[] {  1, 1, 1, 
+                                        4, 1, 1 },
+
+                          new byte[] {  1, 2, 3,
+                                        4, 5, 6 });
+        }
+    }
+}
