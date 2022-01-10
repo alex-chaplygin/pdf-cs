9f7203e72b8cfc41ec7f77bf76107c059c65b885
alex-chaplygin <alex_chaplygin@mail.ru>
2022-01-10 09:11:30 +0300

    JBIG2: точка изображения

--- PdfCS/Filters/JBIG2/JBIG2.cs
+++ PdfCS/Filters/JBIG2/JBIG2.cs
@@ -11,7 +11,22 @@ namespace PdfCS
     /// </summary>
     public class JBIG2
     {
+	/// <summary>
+        /// Ширина изображения
+        /// </summary>
+        private static int width;
+
         /// <summary>
+        /// Высота изображения
+        /// </summary>
+        private static int height;
+
+        /// <summary>
+        /// Данные страницы, 1 бит на точку изображения, точки идут слева-направо, сверху-вниз
+        /// </summary>
+        public static byte[] page;
+
+	/// <summary>
         /// Метод декодирования изображения JBIG2
         /// </summary>
         /// <param name="stream"> Закодированный поток байт</param>
@@ -21,7 +36,20 @@ namespace PdfCS
         /// </returns>
         public static byte[] Decode(byte[] stream, Dictionary<string, object> param= null)
         {
-	    throw new Exception("JBIG 2 не реализован");
+	    JBIG2.height = (int)param["Height"];
+            JBIG2.width = (int)param["Width"];
+            page = new byte[(width / 8) * height];
+            return page;	    
         }
+
+	/// <summary>
+        /// Помещает точку на страницу
+        /// </summary>
+        static void PutPixel(int x, int y)
+        {
+            int bytenumber = y * width + x / 8;
+            int bitnumber = 7 - x % 8;
+            page[bytenumber] = (byte) (page[bytenumber] | 1 << bitnumber);
+        }	
     }
 }
